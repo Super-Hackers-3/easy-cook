d99dd1aca0b7c3d0cfaa327a1952a4a92a96c1b2
thaerbraizat <thaerbraizat13@gmail.com>
2021-04-23 02:30:12 +0300

    add video

---
+++ js/breakf.js
@@ -0,0 +1,75 @@
+'use strict';
+let section=document.getElementsByClassName('FirstBreakFast');
+let button=document.getElementsByClassName('breakFastBtn');
+let idVideo=["quPK4Kf5HrY","Hyz0C7i3ysU","WEDndTCyGgU"];
+let videoSrc=["https://www.youtube.com/embed/quPK4Kf5HrY","https://www.youtube.com/embed/Hyz0C7i3ysU","https://www.youtube.com/embed/WEDndTCyGgU"];
+
+
+for(let i=0; i<3;i++){
+button[i].addEventListener('click', handleClick);
+
+console.log (i);
+console.log(button[i]);
+
+function handleClick(event) {
+    section = document.getElementsByClassName('FirstBreakFast')[i];
+    let ul = document.createElement('ul');
+    section.appendChild(ul);
+    ul.textContent = 'contents';
+
+    for (let x = 1; x < 3; x++) {
+        let li = document.createElement('li')
+        ul.appendChild(li);
+        li.textContent = 'helo';
+    }
+    console.log(button[i]);
+    let iframe = document.createElement('iframe');
+    section.appendChild(iframe);
+   iframe.setAttribute('id', idVideo[i]);
+   document.getElementById( idVideo[i]).src = videoSrc[i];
+   console.log(videoSrc[i]);
+   console.log(idVideo[i]);
+   console.log(i);
+
+    button[i].removeEventListener('click', handleClick);
+    console.log(i);
+    console.log(button[i]);
+}
+}
+// show videos 2
+// let videos = document.getElementsByClassName("youtube"); 
+ 
+// for (let i=0; i<videos.length; i++) {
+  
+//   let youtube = videos[i];
+  
+
+//   let img = document.createElement("img");
+//   img.setAttribute("src", "http://i.ytimg.com/vi/" 
+//                           + youtube.id + "/hqdefault.jpg");
+//   img.setAttribute("class", "thumb");
+  
+ 
+//   let circle = document.createElement("div");
+//   circle.setAttribute("class","circle");  
+  
+//   youtube.appendChild(img);
+//   youtube.appendChild(circle);
+  
+
+//   youtube.onclick = function() {
+ 
+//     let iframe = document.createElement("iframe");
+//     iframe.setAttribute("src",
+//           "https://www.youtube.com/embed/" + this.id 
+//         + "?autoplay=1&autohide=1&border=0&wmode=opaque&enablejsapi=1"); 
+    
+    
+//     iframe.style.width  = this.style.width;
+//     iframe.style.height = this.style.height;
+       
+//     this.parentNode.replaceChild(iframe, this);
+ 
+//   }; 
+// }
+
