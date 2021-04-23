633a5c6b3d3c7ab035fe3f03f121ae8b58fdeebb
ibrahim-alhamshari <ibrahimalhamshari742@gmail.com>
2021-04-23 16:22:23 +0300

    New changes

--- js/breakfast.js
+++ js/breakfast.js
@@ -1,59 +1,36 @@
 'use strict';
 
-// let section = document.getElementById('FirstBreakFast1');
-// let button = document.getElementById('breakFastBtn');
-// button.addEventListener('click', handleClick);
-
-// function handleClick(event) {
-//     let ul = document.createElement('ul');
-//     section.appendChild(ul);
-//     ul.textContent = 'contents';
-
-//     for (let i = 1; i < 5; i++) {
-//         let li = document.createElement('li')
-//         ul.appendChild(li);
-//         li.textContent = 'helo';
-//     }
-//     alert("I am indicating that there is something wrong with your input.")
-// }
-
-
-// // let arrOfVotes = [];
-// // let arrOfShown = [];
-// // function reportResults() {
-// //     let ul = document.getElementById('list');
-// //     for (let i = 0; i < BusMall.allElements.length; i++) {
-// //         arrOfVotes.push(BusMall.allElements[i].votes);
-// //         arrOfShown.push(BusMall.allElements[i].imageTimes);
-// //         let li = document.createElement('li');
-// //         ul.appendChild(li);
-// //         li.textContent = `${BusMall.allElements[i].name} had ${BusMall.allElements[i].votes} votes, and was seen ${BusMall.allElements[i].imageTimes} times.`;
-// //     }
-// //     chart();
-
-
-// //     button.removeEventListener('click', reportResults);
-// // }
-
+let div = document.getElementById('Contets');
 let section=document.getElementsByClassName('FirstBreakFast');
 let button=document.getElementsByClassName('breakFastBtn');
 
+
+// let allElements=[];
+// function BreakFast(name1,name2,name3) {
+//     this.name1 = name1;
+//     this.name2 = name2;
+//     this.name3 =name3;
+//   allElements.push(this);
+//     // console.log(allElements);
+// }
+
+
+
 for(let i=0; i<3;i++){
 button[i].addEventListener('click', handleClick);
-
-console.log (i);
 
 function handleClick(event) {
     section = document.getElementsByClassName('FirstBreakFast')[i];
     let ul = document.createElement('ul');
     section.appendChild(ul);
     ul.textContent = 'contents';
-
-    for (let x = 1; x < 5; x++) {
+    
+    for (let x = 0; x < 4; x++) {
         let li = document.createElement('li')
         ul.appendChild(li);
-        li.textContent = 'helo';
+        li.textContent ='hi';
+        console.log(li.textContent);
     }
-    button[i].removeEventListener('click', handleClick);
-    console.log(i);
+    button[i].removeEventListener('click', handleClick)
 }}
+
